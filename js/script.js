026e804d6ab2a2a5c7d4ee852495cb70a3256fb8
CharlotteWu <charlottewu0754@foxmail.com>
2017-09-16 23:13:17 +0800

    init

---
+++ js/script.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+    /*添加方法*/
+
+    var smb = document.getElementsByClassName('sub-menu-btn');
+    var sml= document.getElementsByClassName('sub-menu-list');
+    var close = document.getElementsByClassName('close');
+
+    smb[0].onclick = function () {
+        sml[0].className = 'sub-menu-list sub-menu-list-popup';
+    }
+
+    close[0].onclick = function () {
+        sml[0].className = 'sub-menu-list';
+    }
+
+//    回到顶部
+    var oTop = document.getElementById("to_top");
+
+    oTop.onclick = function(){
+        document.documentElement.scrollTop = document.body.scrollTop =0;
+    }
+}
